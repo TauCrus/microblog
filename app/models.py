76360f264497de27e7f0ba83cd7a46fee17cea37
TauCrus <TauCrus@163.com>
2019-01-17 18:28:50 +0800

    update forth day work

--- app/models.py
+++ app/models.py
@@ -1,11 +1,38 @@
 from datetime import datetime
 from hashlib import md5
 from time import time
-from flask import current_app
+from flask import current_app, url_for
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 from app import db, login
+
+
+
+# 分页类
+class PaginatedAPIMixin(object):
+    @staticmethod
+    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+        resources = query.paginate(page, per_page, False)
+        data = {
+            'items': [item.to_dict() for item in resources.items],
+            '_meta': {
+                'page': page,
+                'per_page': per_page,
+                'total_pages': resources.pages,
+                'total_items': resources.total
+            },
+            '_links': {
+                'self': url_for(endpoint, page=page, per_page=per_page,
+                                **kwargs),
+                'next': url_for(endpoint, page=page + 1, per_page=per_page,
+                                **kwargs) if resources.has_next else None,
+                'prev': url_for(endpoint, page=page - 1, per_page=per_page,
+                                **kwargs) if resources.has_prev else None
+            }
+        }
+        return data
+
 
 # 关注联系表
 followers = db.Table('followers',
@@ -13,11 +40,8 @@ followers = db.Table('followers',
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 
-class User(UserMixin, db.Model):
+class User(UserMixin, PaginatedAPIMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -25,7 +49,9 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
+    
     followed = db.relationship(
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
@@ -35,28 +61,35 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    # 生成密码
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
+    # 检查密码
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    # 头像
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'http://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
+    # 关注
     def follow(self, user):
         if not self.is_following(user):
             self.followed.append(user)
-
+    
+    # 取消关注
     def unfollow(self, user):
         if self.is_following(user):
             self.followed.remove(user)
-
+    
+    # 判断是否关注
     def is_following(self, user):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
+    # 关注的人的发布
     def followed_posts(self):
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
@@ -64,11 +97,13 @@ class User(UserMixin, db.Model):
         own = Post.query.filter(Post.user_id == self.id)
         return  followed.union(own).order_by(Post.timestamp.desc())
 
+    # 生成重置密码的token
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
             current_app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
 
+    # 验证重置密码的token
     @staticmethod
     def verify_reset_password_token(token):
         try:
@@ -77,7 +112,57 @@ class User(UserMixin, db.Model):
         except:
             return
         return User.query.get(id)
+
+    # 生成用户字典
+    def to_dict(self, include_email=False):
+        data = {
+            'id': self.id,
+            'username': self.username,
+            'last_seen': self.last_seen.isoformat() + 'Z',
+            'about_me': self.about_me,
+            'post_count': self.posts.count(),
+            'follower_count': self.followers.count(),
+            'followed_count': self.followed.count(),
+            '_links':{
+                'self': url_for('api.get_user', id=self.id),
+                'followers': url_for('api.get_followers', id=self.id),
+                'followed': url_for('api.get_followed', id=self.id),
+                'avatar': self.avatar(128)
+            }
+        }
+        if include_email:
+            data['email'] = self.email
+        return data
     
+    # 字典转换到用户对象
+    def from_dict(self, data, new_user=False):
+        for field in ['username', 'email', 'about_me']:
+            if field in data:
+                setattr(self, field, data[field])
+        if new_user and 'password' in data:
+            self.set_password(data['password']) 
+
+    # 为用户返回一个token
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    # 使当前分配的token失效
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    # 校验token
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -88,4 +173,6 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
-
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
